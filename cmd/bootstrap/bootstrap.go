91138d18746b9fafec86245dd21a44f4f8b212a2
Guangbo Chen <steven.guangbo.chen@gmail.com>
2024-09-24 23:08:03 +0800

    fix: add default k8s version and fix path error

--- cmd/bootstrap/bootstrap.go
+++ cmd/bootstrap/bootstrap.go
@@ -5,7 +5,6 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/llmos-ai/llmos/pkg/bootstrap"
-	"github.com/llmos-ai/llmos/pkg/system"
 )
 
 func NewBootstrap() *cobra.Command {
@@ -15,24 +14,26 @@ func NewBootstrap() *cobra.Command {
 }
 
 type Bootstrap struct {
-	Force       bool   `usage:"Run bootstrap even if already bootstrapped" short:"f" env:"LLMOS_BOOTSTRAP_FORCE"`
-	Config      string `usage:"Custom config file path" default:"/etc/llmos/config.yaml" short:"c" env:"LLMOS_CONFIG_FILE"`
-	DataDir     string `usage:"Path to llmos state dir" default:"/var/lib/llmos" env:"LLMOS_DATA_DIR"`
-	Server      string `usage:"Server url to connect to" env:"LLMOS_SERVER"`
-	Role        string `usage:"The node role to join the cluster" enum:"server,agent" short:"r" env:"LLMOS_ROLE"`
-	Token       string `usage:"Token to use for join the cluster" env:"LLMOS_TOKEN"`
-	ClusterInit bool   `usage:"Bootstrap cluster-init role" env:"LLMOS_CLUSTER_INIT"`
+	Force             bool   `usage:"Run bootstrap even if already bootstrapped" short:"f" env:"LLMOS_BOOTSTRAP_FORCE"`
+	Config            string `usage:"Custom config file path" default:"/etc/llmos/config.yaml" short:"c" env:"LLMOS_CONFIG_FILE"`
+	DataDir           string `usage:"Path to llmos state dir" default:"/var/lib/llmos" env:"LLMOS_DATA_DIR"`
+	Server            string `usage:"Server url to connect to" env:"LLMOS_SERVER"`
+	Role              string `usage:"The node role to join the cluster" enum:"server,agent" short:"r" env:"LLMOS_ROLE"`
+	Token             string `usage:"Token to use for join the cluster" env:"LLMOS_TOKEN"`
+	ClusterInit       bool   `usage:"Bootstrap cluster-init role" env:"LLMOS_CLUSTER_INIT"`
+	KubernetesVersion string `usage:"Default kubernetes version to bootstrap" env:"LLMOS_KUBERNETES_VERSION" default:"v1.30.5+k3s1"`
 }
 
 func (b *Bootstrap) Run(cmd *cobra.Command, _ []string) error {
 	boot := bootstrap.New(bootstrap.Config{
-		Force:       b.Force,
-		DataDir:     system.DataDir,
-		ConfigPath:  system.DefaultConfigFile,
-		Server:      b.Server,
-		Role:        b.Role,
-		Token:       b.Token,
-		ClusterInit: b.ClusterInit,
+		Force:             b.Force,
+		DataDir:           b.DataDir,
+		ConfigPath:        b.Config,
+		Server:            b.Server,
+		Role:              b.Role,
+		Token:             b.Token,
+		ClusterInit:       b.ClusterInit,
+		KubernetesVersion: b.KubernetesVersion,
 	})
 	return boot.Run(cmd.Context())
 }
